8fbd508c3ffc171100addcbfb50c3ff93a1eaa11
omoladeajala <shadowcoderx.02@gmail.com>
2026-02-14 16:48:14 +0100

    refactor: remove App component and replace with My favourite food component in main entry point

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default function Hello() {
+  return (
+    <>
+      <h1>My Favourite Food</h1>
+      <ul>
+        <li>Bacon</li>
+        <li>Jamon</li>
+        <li>Noodles</li>
+      </ul>
+    </>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<Hello />);
