ea9433dbdda3d419f8fbf28ea5f51dc718497af7
omoladeajala <shadowcoderx.02@gmail.com>
2026-02-15 03:56:13 +0100

    Refactor code structure for improved readability and maintainability

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,16 +1,19 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import pic10 from "./assets/pic10.jpeg";
+import pic20 from "./assets/pic20.jpeg";
+import pic22 from "./assets/pic22.jpeg";
 
 createRoot(document.getElementById("root")).render(
   <>
     <div>
       <h1 className="heading">My Favourite Food</h1>
 
-      <ul className="list">
-        <li>Bacon</li>
-        <li>Jamon</li>
-        <li>Noodles</li>
-      </ul>
+      <div className="images">
+        <img src={pic10} alt="img" />
+        <img src={pic20} alt="img" />
+        <img src={pic22} alt="img" />
+      </div>
     </div>
   </>,
 );
