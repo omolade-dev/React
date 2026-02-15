d28e2ead4b4b5ab1161de29b9e88d5ec203cd400
omoladeajala <shadowcoderx.02@gmail.com>
2026-02-15 02:43:55 +0100

    update main entry point to display full name and lucky number

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,9 +1,11 @@
 import { createRoot } from "react-dom/client";
 
-const name = "Amala";
-const num = 4;
+const fName = "Omolade";
+const lName = "Ajala";
+const num = 7;
 createRoot(document.getElementById("root")).render(
-  <h1>
-    Hello {name}! Your lucky number is {num}
-  </h1>,
+  <>
+    <h1>My full name is {`${fName} ${lName}`}</h1>
+    <p>Lucky number is {num}</p>
+  </>,
 );
