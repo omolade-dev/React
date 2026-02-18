662b5b1a368383bee24b61572dffa54c667109dd
omoladeajala <shadowcoderx.02@gmail.com>
2026-02-18 02:46:54 +0100

    fix: update greeting logic to display time-based messages

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,3 +1,19 @@
-import { createRoot } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")).render();
+const date = new Date();
+const currentHour = date.getHours();
+
+let greeting;
+const customStyle = {};
+
+if (currentHour < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentHour < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Night";
+}
+
+createRoot(document.getElementById("root")).render(<h1>{greeting}</h1>);
