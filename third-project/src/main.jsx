9e546571b596e43d822ae9472a43661defb7394e
omoladeajala <shadowcoderx.02@gmail.com>
2026-02-19 12:41:22 +0100

    Refactor project: Remove default boilerplate and implement Header component

--- third-project/src/main.jsx
+++ third-project/src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react";
-import * as Calculator from "./calculator.jsx";
+import { add, substract, divide } from "./calculator.jsx";
 
 createRoot(document.getElementById("root")).render(
   <>
     <ul>
-      <li>{Calculator.add(1, 2)}</li>
-      <li>{Calculator.substract(2, 3)}</li>
-      <li>{Calculator.divide(5, 2)}</li>
+      <li>{add(1, 2)}</li>
+      <li>{substract(2, 3)}</li>
+      <li>{divide(5, 2)}</li>
     </ul>
   </>,
 );
